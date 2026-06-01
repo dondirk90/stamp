d8c4d65404faeac6da3295a55491f13d0884be3c
dondirk90 <73251514+dondirk90@users.noreply.github.com>
2026-06-01 19:47:45 +0200

    Lift menu above wallet and enlarge QR render

--- apps/customer-qr-modern.js
+++ apps/customer-qr-modern.js
@@ -2235,7 +2235,7 @@
       try {
         QRCode.toString(
           link,
-          { type: "svg", width: 252, margin: 1, errorCorrectionLevel: "M" },
+          { type: "svg", width: 320, margin: 1, errorCorrectionLevel: "M" },
           function (errSvg, svgText) {
             if (!errSvg && svgText) {
               box.innerHTML = String(svgText);
@@ -2257,7 +2257,7 @@
       try {
         QRCode.toDataURL(
           link,
-          { width: 252, margin: 1, errorCorrectionLevel: "M" },
+          { width: 320, margin: 1, errorCorrectionLevel: "M" },
           function (err, url) {
             if (!err && url) {
               box.innerHTML = "";
@@ -2286,7 +2286,7 @@
       QRCode.toCanvas(
         canvas,
         link,
-        { width: 252, margin: 1, errorCorrectionLevel: "M" },
+        { width: 320, margin: 1, errorCorrectionLevel: "M" },
         function (err) {
           if (err) {
             box.textContent = "QR konnte nicht erzeugt werden.";
