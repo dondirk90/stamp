cd95dd756fff5fec6422a88c9fb08db99bbed95d
dondirk90 <73251514+dondirk90@users.noreply.github.com>
2026-07-31 10:39:29 +0200

    card design freshup

--- apps/pwa/sw-register.js
+++ apps/pwa/sw-register.js
@@ -5,7 +5,7 @@
   if (!(location.protocol === "https:" || location.hostname === "localhost"))
     return;
 
-  var SW_VERSION = "20260731c";
+  var SW_VERSION = "20260731d";
   var reloading = false;
 
   function reloadOnce() {
